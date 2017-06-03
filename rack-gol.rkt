88f75f2f11416fc87527770bc18370438dbbd865
Igor Grebenkov <igor.grebenkov@gmail.com>
2017-06-03 05:56:02 -0400

    can add cells with leftclick+drag and delete with rightclick+drag

--- rack-gol.rkt
+++ rack-gol.rkt
@@ -1,29 +1,44 @@
 #lang racket
 (require racket/gui)
 (require racket/draw)
-(require math/array)
 
-; The length of the sides of each cell
-(define c-length 20)
-(define frame-height 1000)
+(define cell-length 20)           
+(define frame-height 1000)      
 (define frame-width 1000)
 (define color-black (make-object color% 0 0 0))
-(define color-red-str "red")
-(define color-green-str "green")
+(define color-dead-str "black")
+(define color-alive-str "green")
 
-(define (mouse-click-action x y)
-  (let ((cell-x (exact-floor (/ x c-length)))
-        (cell-y (exact-floor (/ y c-length))))
-    (cell-toggle-status cell-ht (list cell-x cell-y))))
+(define (mouse-click-action x y action)
+  (let ((cell-x (exact-floor (/ x cell-length)))
+        (cell-y (exact-floor (/ y cell-length))))
+    (cond ((equal? action 'left-click)
+           (cell-toggle-status cell-ht (list cell-x cell-y)))
+          ((equal? action 'give-life)
+           (cell-update-status cell-ht (list cell-x cell-y) 'alive))
+          ((equal? action 'kill)
+           (cell-update-status cell-ht (list cell-x cell-y) 'dead)))))
 
 (define game-canvas%
   (class canvas%
     (define/override (on-event event)
-      (cond ((or (send event button-down? 'left)
-                 (send event get-left-down))
-             (let ((x (send event get-x))
-                   (y (send event get-y)))
-               (mouse-click-action x y)))
+      (cond ((send event button-down? 'left)
+               (mouse-click-action
+                (send event get-x)
+                (send event get-y)
+                'left-click))
+            ((and (send event get-left-down)
+                 (send event dragging?))
+             (mouse-click-action
+              (send event get-x)
+              (send event get-y)
+              'give-life))
+            ((and (send event get-right-down)
+                  (send event dragging?))
+             (mouse-click-action
+              (send event get-x)
+              (send event get-y)
+              'kill))
             (else #f)))
       (super-new)))
   
@@ -32,32 +47,30 @@
                    [width frame-height]
                    [height frame-width]))
 
-(define canvas (new game-canvas%	 
+(define board-canvas (new game-canvas%	 
                     [parent frame]	 
                     [style '()]
                     [paint-callback
                      (lambda (canvas dc)
                        (send canvas set-canvas-background color-black))]))
 
-(define dc (send canvas get-dc))
+(define dc (send board-canvas get-dc))
 
 (send frame show #t)
 (sleep/yield 1)
 
 ; Finds the max value for the x/y co-ordinates
 (define max-xy
-  (exact-round (/ frame-height c-length)))
+  (exact-round (/ frame-height cell-length)))
 
 ; Draws a single square on the canvas at (x,y)
-(define (draw-square x y length color)
+(define (draw-square x y color)
   (begin
     (send dc set-brush (make-object brush% color 'solid))
     (send dc draw-rectangle
-          (* x length)
-          (* y length)
-          length length)))
-
-
+          (* x cell-length)
+          (* y cell-length)
+          cell-length cell-length)))
 
 ; Hash table for the cells
 (define cell-ht (make-hash))
@@ -67,18 +80,19 @@
   (begin
     (hash-set! ht key status)
     (cond ((equal? status 'alive)
-           (draw-square (car key) (cadr key) c-length color-green-str))
+           (draw-square (car key) (cadr key) color-alive-str))
           ((equal? status 'dead)
-           (draw-square (car key) (cadr key) c-length color-red-str)))))
+           (draw-square (car key) (cadr key) color-dead-str)))))
 
+; Toggles a cell's status and draws it on the canvas
 (define (cell-toggle-status ht key)
   (let ((status (hash-ref ht key)))
     (cond ((equal? status 'alive)
            (hash-set! ht key 'dead)
-           (draw-square (car key) (cadr key) c-length color-red-str))
+           (draw-square (car key) (cadr key) color-dead-str))
           ((equal? status 'dead)
            (hash-set! ht key 'alive)
-           (draw-square (car key) (cadr key) c-length color-green-str)))))
+           (draw-square (car key) (cadr key) color-alive-str)))))
 
 ; List of all the cell keys in the hash table
 (define cell-keys '())
@@ -92,6 +106,7 @@
                  (cons (list i j) cell-keys))))
     cell-keys))
 
+; Populates the hash table with every cell initially dead
 (define (populate-table ht key-list)
   (cond ((null? key-list) #t)
         (else
@@ -99,28 +114,7 @@
            (begin
              (hash-set! ht key 'dead)
              (populate-table ht (cdr key-list)))))))
-
-
-(define (populate-random-alive ht key-list)
-  (cond ((null? key-list) #t)
-        (else
-         (let ((key (car key-list))
-               (rand (random 1 10 (make-pseudo-random-generator))))
-           (begin
-             (cond ((or (equal? rand 2) (equal? rand 5) (equal? rand 7) (equal? rand 9))
-                    (hash-set! ht key 'alive)
-                    (populate-random-alive ht (cdr key-list)))
-                   (else
-                    (populate-random-alive ht (cdr key-list)))))))))          
-
-
-(define (init-alive ht list)
-  (cond ((null? list) #t)
-        (else
-         (hash-set! ht (car list) 'alive)
-         (init-alive ht (cdr list)))))
-
-
+  
 ; Draws the cells in the coord-list to the canvas
 (define (draw-cells ht key-list)
   (cond ((null? key-list) #t)
@@ -142,7 +136,6 @@
 
 
 (populate-table cell-ht (gen-xy max-xy))
-(populate-random-alive cell-ht (gen-xy max-xy))
 (draw-cells cell-ht (gen-xy max-xy))
 
 
