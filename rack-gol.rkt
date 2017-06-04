a5c3762f65e27704276e8ef03c51cd593c287073
Igor Grebenkov <igor.grebenkov@gmail.com>
2017-06-04 05:07:15 -0400

    rewrote table init function to directly load the hash table without an intermediate list

--- rack-gol.rkt
+++ rack-gol.rkt
@@ -5,16 +5,18 @@
 ; Constants
 (define INIT-FRAME-HEIGHT 700)
 (define INIT-FRAME-WIDTH 1000)
-(define cell-length 15)           
+(define INIT-CELL-LENGTH 20)
+(define INIT-SLEEP-DELAY (/ 1 25))
 (define color-black (make-object color% 0 0 0))
 (define color-dead-str "black")
-(define color-alive-str "red")
-(define sleep-delay (/ 1 10))
+(define color-alive-str "green")
 (define sim-started 'false)
 
 ; State variables
+(define cell-length INIT-CELL-LENGTH)  
 (define frame-height INIT-FRAME-HEIGHT)      
 (define frame-width INIT-FRAME-WIDTH)
+(define sleep-delay INIT-SLEEP-DELAY)
 (define max-x
   (exact-round (/ frame-width cell-length)))
 (define max-y
@@ -25,71 +27,35 @@
 
 ; %%%%%%%%%%%%%%%%%%%%%%%%%%%%%% STATE %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-; Generates a list of all possible cell keys
-(define (gen-xy max-x max-y)
-  (begin
-    (do ((i max-x (sub1 i))) ((< i 0))
-      (do ((j max-y (sub1 j))) ((< j 0))
-           (set! cell-keys
-                 (cons (list i j) cell-keys))))))
+; Populates the hash table with dead cells
+(define (cell-init-table ht max-x max-y)
+  (for ([i (in-range 0 (add1 max-x))])
+    (for ([j (in-range 0 (add1 max-y))])
+      (let ((key (list i j)))
+        (hash-set! ht key 'dead)))))
 
-; Populates the hash table with every cell initially dead
-(define (init-table ht key-list)
-  (cond ((null? key-list) #t)
-        (else
-         (let ((key (car key-list)))
-           (begin
-             (hash-set! ht key 'dead)
-             (init-table ht (cdr key-list)))))))
-
-
-(define (resize-table ht curr-x curr-y)
+; Increases the size of the table
+; Used to dynamically adjust the table size when resizing the frame
+(define (cell-resize-table ht curr-x curr-y)
   (let ((new-max-x (exact-round (/ frame-width cell-length)))
         (new-max-y (exact-round (/ frame-height cell-length))))
-    (cond ((and (> new-max-x curr-x) (> new-max-y curr-y))
-           ;(define temp2 (list new-max-x new-max-y))
-           ;(printf "x: ~a, y: ~a #\n" (first temp2) (second temp2))
+    (cond ((and (> new-max-x curr-x) (> new-max-y curr-y)) ; height/width increased
            (for ([i (in-range curr-x (add1 new-max-x))])
-             ;(printf "#\n")
              (for ([j (in-range 0 (add1 new-max-y))])
-               ;(define temp (list i j))
-               ;(printf "(~a ~a) " (first temp) (second temp))
-               ;(hash-set! ht (list i j) 'dead)
-                      ;(printf "(~a ~a) " (car (list j i)) (cadr (list j i)))
                (hash-set! ht (list i j) 'dead)))
            (for ([i (in-range 0 (add1 new-max-x))])
-             (for ([j (in-range curr-y (add1 new-max-y))])
+             (for ([j (in-range curr-y (add1 new-max-y))]) 
                (hash-set! ht (list i j) 'dead))))
-           ;(printf "#\n"))
-          ((> new-max-x curr-x)
+          ((> new-max-x curr-x)                            ; width increased only
            (for ([i (in-range curr-x (add1 new-max-x))])
              (for ([j (in-range 0 (add1 curr-y))])
                (hash-set! ht (list i j) 'dead))))
-          ((> new-max-y curr-y)
+          ((> new-max-y curr-y)                            ; height increased only
            (for ([i (in-range 0 (add1 curr-x))])
              (for ([j (in-range curr-y (add1 new-max-y))])
                (hash-set! ht (list i j) 'dead)))))
     (set! max-x new-max-x)
     (set! max-y new-max-y)))
-
-
-(define (grow-table ht x y)
-  (let ((new-max-x (exact-round (/ frame-width cell-length)))
-        (new-max-y (exact-round (/ frame-height cell-length))))
-  (for ([i (in-range 0 new-max-x)])
-    (for ([j (in-range 0 new-max-y)])
-      (let* ((key (list i j)))
-        (cond ((not (hash-has-key? ht key))
-               (hash-set! ht key 'dead))
-              (else
-               (hash-set! ht key (hash-ref ht key))))))
-  (set! max-x new-max-x)
-  (set! max-y new-max-y))))
-       
-(define (update-table ht)
-  (for ([(key status) (in-hash ht)])
-    (cond ((not (hash-has-key? ht key))
-           (hash-set! ht key 'dead)))))
 
 ; Generates a list of the Moore neighborhood of a cell
 (define (cell-neighbors-moore key)
@@ -216,10 +182,10 @@
               (send event get-y)
               'kill))
             (else #f)))
-    (define/override (on-char event)
+    (define/override (on-char event)                     ; Keyboard events
       (let ((key-char (send event get-key-code)))
            (cond ((equal? key-char 'f1) (start-loop))))) ; start the simulation with f1
-    (define/override (on-paint)
+    (define/override (on-paint)                          ; on-paint override
       (begin
         (send board-canvas set-canvas-background color-black)
         (let ((curr-frame-height (send frame get-height))
@@ -227,16 +193,9 @@
         (cond ((and (equal? sim-started 'true)
                (not (and (equal? curr-frame-height INIT-FRAME-HEIGHT)  ; handles window resizing
                          (equal? curr-frame-width INIT-FRAME-WIDTH))))
-               ;(printf "~a #\n" curr-frame-height)
                (set! frame-height curr-frame-height)
                (set! frame-width curr-frame-width)
-                   ;(displayln '_)
-                   ;(displayln new-max-x)
-                   ;(set! cell-keys '())
-                   ;(gen-xy max-x max-y)
-                   ;(update-table cell-ht)
-                   (resize-table cell-ht max-x max-y)
-                   
+                   (cell-resize-table cell-ht max-x max-y)
                    (draw-board cell-ht))))))
     (super-new)))
 
@@ -257,6 +216,7 @@
         (y (cadr key)))
   (begin
     (send dc set-brush (make-object brush% color 'solid))
+    (send dc set-pen (make-object pen% color-black 0 'solid))
     (send dc draw-rectangle
           (* x cell-length)
           (* y cell-length)
@@ -271,12 +231,12 @@
 (send frame show #t)
 (sleep/yield 0)
 
-; Initialize hash table with dead cells and draw it
-(gen-xy max-x max-y)
-(init-table cell-ht cell-keys)
-(set! cell-buf (hash-copy cell-ht))
-(draw-board cell-ht)
+; Initialization
+(cell-init-table cell-ht max-x max-y)     
+(set! cell-buf (hash-copy cell-ht))  
+(draw-board cell-ht)                 
 (set! sim-started 'true)
+
 ; Produces one iteration of the game
 (define (game-one-iter)
   (begin
